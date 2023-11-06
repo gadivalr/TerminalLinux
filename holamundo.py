b7f8555d1199f0eeaf9c4874da5a4e29b0031cd5
gadivalr <levi.sojos7@gmail.com>
2023-11-06 01:01:20 -0300

    add  usuario2

---
+++ holamundo.py
@@ -0,0 +1,11 @@
+print("Hello World!")
+n = input('\033[33mIngrese su nombre de usario\033[0m:  ')
+a = input('\033[33mIngrese su contraseña\033[0m: ')
+print(f"bienvenido {n}, su contraseña es: ")
+i=0
+for i in range(5):
+	print(f'{a}')
+	i+=1
+
+
+
